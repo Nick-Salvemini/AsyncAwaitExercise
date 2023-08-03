7cf9ee12dc09fbd4775b987243f2276ae1bfe868
Nick Salvemini <salvemini.nick@gmail.com>
2023-08-03 15:28:01 -0400

    completed exercise

--- cards.js
+++ cards.js
@@ -1,10 +1,10 @@
 // 1
 async function drawACard(num) {
     let res = await $.getJSON(`https://deckofcardsapi.com/api/deck/new/draw/?count=${num}`);
-    console.log(`${res.cards[0].value} of ${res.cards[0].suit}`)
+    console.log(`Part 2.1: ${res.cards[0].value} of ${res.cards[0].suit}`)
 }
 
-// drawACard(1)
+drawACard(1)
 
 // 2
 async function drawTwoCards() {
@@ -13,10 +13,10 @@ async function drawTwoCards() {
     let card1 = `${draw1.cards[0].value} of ${draw1.cards[0].suit}`
     let draw2 = await $.getJSON(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
     let card2 = `${draw2.cards[0].value} of ${draw2.cards[0].suit}`
-    console.log(`${card1} and ${card2}`)
+    console.log(`Part 2.2: ${card1} and ${card2}`)
 }
 
-// drawTwoCards()
+drawTwoCards()
 
 //3
 let deckId
@@ -24,21 +24,15 @@ let deckId
 async function shuffleDeck() {
     let shuffleDeck = await $.getJSON(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
     let shuffleDeckId = shuffleDeck.deck_id;
-
-
     deckId = shuffleDeckId
-
-    console.log(shuffleDeckId)
-    console.log(deckId)
 }
 
 shuffleDeck()
 
-
-
 async function drawCard() {
-    let card = await $.getJSON(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-    console.log(`${card.cards[0].value} of ${card.cards[0].suit}`)
+    let draw = await $.getJSON(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+    let card = `${draw.cards[0].value} of ${draw.cards[0].suit}`;
+    $(`<li>${card}</li>`).prependTo('#cardsList')
 }
 
-$('#drawACard').on('click', drawCard())
+$('#drawACard').on('click', drawCard)
--- numbers.js
+++ numbers.js
@@ -3,7 +3,7 @@ let url = `http://numbersapi.com/`;
 // 1
 async function getNumFact(num) {
     let res = await $.getJSON(`${url}${num}?json`);
-    console.log(res.text)
+    console.log('Part 1.1: ', res.text)
 }
 
 getNumFact(3)
@@ -11,7 +11,7 @@ getNumFact(3)
 // 2
 async function getNumFactWithArray(arr) {
     let res = await $.getJSON(`${url}${arr}?json`);
-    console.log(res)
+    console.log('Part 1.2: ', res)
 }
 
 getNumFactWithArray([3, 6, 9])
@@ -26,7 +26,7 @@ async function getFourNumFacts(num) {
         $.getJSON(`${url}${num}?json`)
     ])
 
-    console.log(res[0].text, res[1].text, res[2].text, res[3].text)
+    console.log('Part 1.3: ', res[0].text, res[1].text, res[2].text, res[3].text)
 }
 
 getFourNumFacts(3)
